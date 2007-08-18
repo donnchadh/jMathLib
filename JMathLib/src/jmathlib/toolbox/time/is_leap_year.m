aea1097e9d28b6b186d006c8813ef27e65b05dcb
st_mueller <st_mueller@0cae5b43-3635-0410-beba-a737c0e5ce6c>
2007-08-18 11:37:57 +0000

    Initial import.

---
+++ JMathLib/src/jmathlib/toolbox/time/is_leap_year.m
@@ -0,0 +1,50 @@
+## Copyright (C) 1996, 1997 John W. Eaton
+##
+## This file is part of Octave.
+##
+## Octave is free software; you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2, or (at your option)
+## any later version.
+##
+## Octave is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with Octave; see the file COPYING.  If not, write to the Free
+## Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+## 02110-1301, USA.
+
+## -*- texinfo -*-
+## @deftypefn {Function File} {} is_leap_year (@var{year})
+## Return 1 if the given year is a leap year and 0 otherwise.  If no
+## arguments are provided, @code{is_leap_year} will use the current year.
+## For example,
+##
+## @example
+## @group
+## is_leap_year (2000)
+##      @result{} 1
+## @end group
+## @end example
+## @end deftypefn
+
+## Author: jwe
+
+function retval = is_leap_year (year)
+
+  if (nargin > 1)
+    print_usage ();
+  endif
+
+  if (nargin == 0)
+    t = clock ();
+    year = t (1);
+  endif
+
+  retval = ((rem (year, 4) == 0 & rem (year, 100) != 0) ...
+            | rem (year, 400) == 0);
+
+endfunction
---
+++ JMathLib/src/jmathlib/toolbox/time/toc.java
@@ -0,0 +1,65 @@
+package jmathlib.toolbox.time;
+
+/* This file is part or MATHLIB */
+
+import jmathlib.core.tokens.*;
+import jmathlib.core.tokens.numbertokens.DoubleNumberToken;
+import jmathlib.core.functions.ExternalFunction;
+import java.util.*;
+
+/*
+Author: Stefan Mueller (stefan@held-mueller.de) 2002
+*/
+
+
+/**An external function for computing the time difference between a 
+   call to tic() and toc()  (internal stop watch)  */
+public class toc extends ExternalFunction
+{
+	/**returns a time difference 
+	* @return the time difference in seconds as a double number */
+	public OperandToken evaluate(Token[] operands)
+	{
+
+		Date d = new Date();
+        
+        double stop = (double)d.getTime();
+        
+        if (!getVariables().isVariable("_tic"))
+        	throwMathLibException("toc: you must call tic before toc");
+            
+   	 	OperandToken ticTok = getVariable("_tic").getData();
+		
+        if (ticTok instanceof DoubleNumberToken)
+        {
+        	double start = ((DoubleNumberToken)ticTok).getValueRe();
+            return new DoubleNumberToken( (stop - start)/1000 );
+        }
+        else
+            throwMathLibException("toc: _tic variable has wrong type"); 	
+            
+        return null; 
+
+	} // end eval
+}
+
+
+/*
+@GROUP
+time
+@SYNTAX
+toc()
+@DOC
+Returns the time difference in seconds between the call to tic()
+and toc().
+@EXAMPLES
+<programlisting>
+tic()
+toc()
+returns
+ans = 1.34
+</programlisting>
+@NOTES
+@SEE
+tic, pause, date, time
+*/
