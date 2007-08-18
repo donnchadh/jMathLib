25219a81f37630eb34fb931b58d9d8a8130a0a5d
st_mueller <st_mueller@0cae5b43-3635-0410-beba-a737c0e5ce6c>
2007-08-18 17:47:43 +0000

    Initial import.

---
+++ JMathLib/trunk/src/jmathlib/toolbox/io/runfile.java
@@ -0,0 +1,84 @@
+package jmathlib.toolbox.io;
+
+import jmathlib.core.tokens.Token;
+import jmathlib.core.tokens.OperandToken;
+import jmathlib.core.tokens.CharToken;
+import jmathlib.core.functions.ExternalFunction;
+import jmathlib.core.interpreter.ErrorLogger;
+import java.io.*;
+
+/**An external function for loading and running m-files  (script-files)      *
+ * This function is only for script-files not for function-files             */
+public class runfile extends ExternalFunction
+{
+	/** Check that the operand is a string then open the file                *
+	 *  referenced.                                                          *
+         * @param operands[0] string which specifies the function to load    */
+	public OperandToken evaluate(Token[] operands)
+	{
+
+		String answerString="";
+		String lineFile="";
+		String line=" ";
+
+
+		// One operand of type CharToken (e.g. runfile("test") )
+        if (getNArgIn(operands) != 1)
+			throwMathLibException("RunFile: number of arguments != 1");
+
+		if (!(operands[0] instanceof CharToken)) 
+        	throwMathLibException("RunFile: argument must be a string");
+		
+		String fileName = ((CharToken)operands[0]).toString();
+			
+		// If the filename doesn't have an extension add the default 
+		//     extension of .m
+		if(fileName.indexOf(".m") == -1)	fileName += ".m";
+			
+
+		File scriptFile = new File(getWorkingDirectory(), fileName);
+
+		if(!scriptFile.exists()) return null;
+
+		ErrorLogger.debugLine("loading >"+fileName+"<");
+
+		try 
+		{			
+			// load file 
+			BufferedReader inReader = new BufferedReader( new FileReader(fileName));
+			while ( line != null)
+			{
+				line= inReader.readLine();	    	
+				lineFile += line;
+			}
+			inReader.close();					
+
+			//execute the file and store the answer
+			answerString = getInterpreter().executeExpression(lineFile);		
+
+		}
+		catch (Exception e)
+		{
+			ErrorLogger.debugLine("RunFile: load function exception");
+		}		    
+
+		
+		return null;
+	}
+}
+
+/*
+@GROUP
+IO
+@SYNTAX
+RUNFILE("filename")
+@DOC
+Runs the script file specified by filename.
+@NOTE
+This is used to run script files, not function files.
+@EXAMPLES
+RUNFILE("script.m");
+@SEE
+dir, cd, systemcommand
+*/
+
---
+++ JMathLib/trunk/src/jmathlib/toolbox/io/savevariables.java
@@ -0,0 +1,52 @@
+package jmathlib.toolbox.io;
+
+import jmathlib.core.tokens.*;
+import jmathlib.core.functions.ExternalFunction;
+import java.io.*;
+
+public class savevariables extends ExternalFunction
+{
+	public OperandToken evaluate(Token[] operands)
+	{
+
+        if (getNArgIn(operands) > 1)
+			throwMathLibException("savevariables: number of arguments > 1");
+
+
+        String file = "." + File.separator + "variables.mlf";
+        
+        if (getNArgIn(operands) == 1)
+        {
+            if (!(operands[0] instanceof CharToken)) 
+                throwMathLibException("savevariables: argument must be a string");
+
+            file = ((CharToken)operands[0]).toString();
+        }
+
+        getVariables().saveVariables(file);
+        
+		return null;		
+
+	} // end eval
+}
+
+/*
+@GROUP
+IO
+@SYNTAX
+savevariables()
+savevariables("name.mlf")
+@DOC
+saves the variables from the current workspace into a serialized .mlf-file
+@EXAMPLES
+<programlisting>
+savevariables()
+</programlisting>
+@NOTES
+The variables are written as a serialized stream of java objects. Therefor the
+format of the .mlf-file is java-specific. The file is also specific to the
+versions of MathLib token and classes.
+@SEE
+loadvariables
+*/
+
