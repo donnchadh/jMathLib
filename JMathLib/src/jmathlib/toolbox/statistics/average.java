31c98e480827e0aa0849720a06c7cfbbfd9695e2
st_mueller <st_mueller@0cae5b43-3635-0410-beba-a737c0e5ce6c>
2007-08-18 11:31:18 +0000

    Initial import.

---
+++ JMathLib/src/jmathlib/toolbox/statistics/average.java
@@ -0,0 +1,45 @@
+package jmathlib.toolbox.statistics;
+
+import jmathlib.core.functions.*;
+import jmathlib.core.tokens.*;
+import jmathlib.core.tokens.numbertokens.DoubleNumberToken;
+
+
+
+/**
+ * An external function for calculating the average
+ */
+public class average extends ExternalFunction
+{
+
+    public OperandToken evaluate(Token[] operands)
+    {
+        int i;
+        OperandToken ot = (OperandToken) operands[0];
+
+        for (i=1; i<operands.length; i++)
+            {
+                ot.add((OperandToken)operands[i]);
+            }
+        ot.divide(new DoubleNumberToken(i));
+
+        return ot;
+    }
+}
+
+
+
+/*
+@GROUP
+statistics
+@SYNTAX
+average(1,2,3,...,n)
+@DOC
+Calculates the average of the passed parameters.
+@EXAMPLES
+<programlisting>
+Average(a,b,3,4)
+</programlisting>
+@SEE
+std, mean, var
+*/
