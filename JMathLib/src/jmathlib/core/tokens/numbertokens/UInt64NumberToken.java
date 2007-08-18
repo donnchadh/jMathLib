155de5a61f04afbae91216f54eeadf3c338496cc
st_mueller <st_mueller@0cae5b43-3635-0410-beba-a737c0e5ce6c>
2007-08-18 09:52:05 +0000

    Initial import.

---
+++ JMathLib/src/jmathlib/core/tokens/numbertokens/UInt64NumberToken.java
@@ -0,0 +1,9 @@
+package jmathlib.core.tokens.numbertokens;
+
+import jmathlib.core.tokens.NumberToken;
+
+
+public class UInt64NumberToken extends NumberToken
+{            
+
+} // end UInt64NumberToken
