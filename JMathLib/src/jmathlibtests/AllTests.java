a8d0c56987fbd4dc717f9866b4904bbb4a8d59ba
st_mueller <st_mueller@0cae5b43-3635-0410-beba-a737c0e5ce6c>
2007-08-18 12:17:12 +0000

    Initial import.

---
+++ JMathLib/src/jmathlibtests/AllTests.java
@@ -0,0 +1,28 @@
+package jmathlibtests;
+
+import jmathlib.tools.junit.framework.*;
+
+/**
+ * TestSuite that runs all the tests
+ *
+ */
+public class AllTests {
+
+	public static void main (String[] args) {
+	    jmathlib.tools.junit.textui.TestRunner.run (suite());
+	}
+	public static Test suite ( ) {
+		TestSuite suite= new TestSuite("JMathLib Tests");
+
+		/* include subdirectories here */
+        suite.addTest(jmathlibtests.core.AllTests.suite());
+		suite.addTest(jmathlibtests.toolbox.AllTests.suite());
+        
+
+		/* include tests in this directory here */
+		//suite.addTest(MathLib.Tools.TestSuite.Interpreter.testParser.suite());
+
+	    return suite;
+	}
+}
+
---
+++ JMathLib/src/jmathlibtests/toolbox/AllTests.java
@@ -0,0 +1,27 @@
+package jmathlibtests.toolbox;
+
+import jmathlib.tools.junit.framework.*;
+
+/**
+ * TestSuite that runs all the tests
+ *
+ */
+public class AllTests {
+
+	public static void main (String[] args) {
+	    jmathlib.tools.junit.textui.TestRunner.run (suite());
+	}
+	public static Test suite ( ) {
+		TestSuite suite= new TestSuite("toolbox");
+
+		/* include subdirectories here */
+		//suite.addTest(jmathlibtests.toolbox.AllTests.suite());
+        
+
+		/* include tests in this directory here */
+		//suite.addTest(MathLib.Tools.TestSuite.Interpreter.testParser.suite());
+
+	    return suite;
+	}
+}
+
