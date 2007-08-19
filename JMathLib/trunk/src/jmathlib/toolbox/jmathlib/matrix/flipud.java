981fc295d29ae1ac3fbf4fca0dcc28c5d8265ac9
st_mueller <st_mueller@0cae5b43-3635-0410-beba-a737c0e5ce6c>
2007-08-19 13:02:51 +0000

    Initial import.

---
+++ JMathLib/trunk/src/jmathlib/toolbox/jmathlib/matrix/flipud.java
@@ -0,0 +1,75 @@
+package jmathlib.toolbox.jmathlib.matrix;
+
+import jmathlib.core.tokens.Token;
+import jmathlib.core.tokens.OperandToken;
+import jmathlib.core.tokens.numbertokens.DoubleNumberToken;
+import jmathlib.core.functions.ExternalFunction;
+
+/**An external function for flipping matrices from up to down */
+public class flipud extends ExternalFunction
+{
+	/**return a  matrix 
+	@param operands[0] = matrix to flip from up to down */
+	public OperandToken evaluate(Token[] operands)
+	{
+
+		// one operands (e.g. flipud(A) )
+		if (operands == null)                             
+            return null;
+
+		if (operands.length != 1)                         
+            return null;
+
+		if (operands[0] == null)       
+			return null;
+
+		if (!(operands[0] instanceof DoubleNumberToken))
+			return null;
+
+
+		// get data from array
+		double[][] real =      ((DoubleNumberToken)operands[0]).getValuesRe();
+        double[][] imag =      ((DoubleNumberToken)operands[0]).getValuesIm();
+
+		int dy     = real.length;
+        int dx     = real[0].length;	
+	
+        double re = 0.0;
+        double im = 0.0;
+    
+		// flip matrix
+		for (int xi=0; xi<dx ; xi++)
+		{
+			for (int yi=0; yi<dy/2 ; yi++)
+			{
+				// flip
+				re = real[yi][xi];
+	            real[yi][xi]      = real[dy-1-yi][xi];
+                real[dy-1-yi][xi] = re; 
+
+				im = imag[yi][xi];
+	            imag[yi][xi]      = imag[dy-1-yi][xi];
+                imag[dy-1-yi][xi] = im; 
+                
+			}
+        }
+		return new DoubleNumberToken(real, imag);		
+
+	} // end eval
+}
+
+/*
+@GROUP
+matrix
+@SYNTAX
+flipud(A)
+@DOC
+Flips a matrix from up to down.
+@NOTES
+@EXAMPLES
+flipud([1,2,3;4,5,6]) = [4,5,6;1,2,3]
+@SEE
+fliplr
+
+*/
+
