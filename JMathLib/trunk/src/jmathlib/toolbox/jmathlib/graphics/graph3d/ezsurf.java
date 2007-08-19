a4c1a5f7eebdfe5cefb1f2547aad7aaddec4c0fa
st_mueller <st_mueller@0cae5b43-3635-0410-beba-a737c0e5ce6c>
2007-08-19 08:21:36 +0000

    Initial import.

---
+++ JMathLib/trunk/src/jmathlib/toolbox/jmathlib/graphics/graph3d/ezsurf.java
@@ -0,0 +1,17 @@
+package jmathlib.toolbox.jmathlib.graphics.graph3d;
+
+import jmathlib.core.tokens.*;
+import jmathlib.core.functions.ExternalFunction;
+
+public class ezsurf extends ExternalFunction
+{
+
+	public OperandToken evaluate(Token[] operands)
+	{
+
+		throwMathLibException("ezsurf: not implemented yet");
+
+		return null;
+	}
+}
+
---
+++ JMathLib/trunk/src/jmathlib/toolbox/jmathlib/graphics/graph3d/mesh.java
@@ -0,0 +1,17 @@
+package jmathlib.toolbox.jmathlib.graphics.graph3d;
+
+import jmathlib.core.tokens.*;
+import jmathlib.core.functions.ExternalFunction;
+
+public class mesh extends ExternalFunction
+{
+
+	public OperandToken evaluate(Token[] operands)
+	{
+
+		throwMathLibException("mesh: not implemented yet");
+
+		return null;
+	}
+}
+
