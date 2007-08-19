92e974295801fe1278d13943a741782bf5ce19fe
st_mueller <st_mueller@0cae5b43-3635-0410-beba-a737c0e5ce6c>
2007-08-19 07:11:43 +0000

    Initial import.

---
+++ JMathLib/trunk/src/jmathlib/toolbox/general/int16.java
@@ -0,0 +1,73 @@
+package jmathlib.toolbox.general;
+
+import jmathlib.core.tokens.*;
+import jmathlib.core.tokens.numbertokens.*;
+import jmathlib.core.functions.ExternalFunction;
+
+public class int16 extends ExternalFunction
+{
+	public OperandToken evaluate(Token[] operands)
+	{
+
+        if (getNArgIn(operands) != 1 )
+			throwMathLibException("int16: number of arguments !=1");
+        
+        if (!(operands[0] instanceof DoubleNumberToken))
+            throwMathLibException("int16: only works on numbers");
+
+        DoubleNumberToken num = (DoubleNumberToken)operands[0];
+        
+        int[] size = num.getSize();
+        
+        int n = num.getNumberOfElements();
+        
+        Int16NumberToken int16 = new Int16NumberToken(size, null, null);
+
+        double re  = 0;
+        double im  = 0;
+        short  reI = 0;
+        short  imI = 0;
+        for (int i=0; i<n; i++)
+        {
+
+            re = num.getValueRe(i);
+            im = num.getValueIm(i);
+                
+            if (re>32767)      //(2^15-1)
+                reI = 32767;
+            else if (re<-32768) // (-2^15)
+                reI = -32768;
+            else
+                reI = (short)re;
+
+            if (im>32767)
+                imI = 32767;
+            else if (im<-32768)
+                imI = -32768;
+            else
+                imI = (short)im;
+
+            int16.setValue(i, reI, imI);
+            
+        }
+        
+        return int16;
+        
+	} // end eval
+}
+
+/*
+@GROUP
+general
+@SYNTAX
+int16(x)
+@DOC
+converts a double array into an array of int16 (range -32768=-2^25 up to +32767=+2^15-1)
+@EXAMPLES
+<programlisting>
+
+</programlisting>
+@SEE
+double, int8, uint8, uint16
+*/
+
