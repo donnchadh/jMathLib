56126dfd95622c83f314a23c3cd7538a41bfb325
st_mueller <st_mueller@0cae5b43-3635-0410-beba-a737c0e5ce6c>
2007-08-19 10:59:09 +0000

    Initial import.

---
+++ JMathLib/trunk/src/jmathlib/toolbox/jmathlib/graphics/graph3d/patch.java
@@ -0,0 +1,17 @@
+package jmathlib.toolbox.jmathlib.graphics.graph3d;
+
+import jmathlib.core.tokens.*;
+import jmathlib.core.functions.ExternalFunction;
+
+public class patch extends ExternalFunction
+{
+
+	public OperandToken evaluate(Token[] operands)
+	{
+
+		throwMathLibException("patch: not implemented yet");
+
+		return null;
+	}
+}
+
---
+++ JMathLib/trunk/src/jmathlib/toolbox/jmathlib/graphics/graph3d/pie3.java
@@ -0,0 +1,17 @@
+package jmathlib.toolbox.jmathlib.graphics.graph3d;
+
+import jmathlib.core.tokens.*;
+import jmathlib.core.functions.ExternalFunction;
+
+public class pie3 extends ExternalFunction
+{
+
+	public OperandToken evaluate(Token[] operands)
+	{
+
+		throwMathLibException("pie3: not implemented yet");
+
+		return null;
+	}
+}
+
