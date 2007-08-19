1536afc1d492327bc480d6b9369249f0ca2f3fbf
st_mueller <st_mueller@0cae5b43-3635-0410-beba-a737c0e5ce6c>
2007-08-19 08:20:34 +0000

    Initial import.

---
+++ JMathLib/trunk/src/jmathlib/toolbox/jmathlib/graphics/graph2d/plotyy.java
@@ -0,0 +1,17 @@
+package jmathlib.toolbox.jmathlib.graphics.graph2d;
+
+import jmathlib.core.tokens.*;
+import jmathlib.core.functions.ExternalFunction;
+
+public class plotyy extends ExternalFunction
+{
+
+	public OperandToken evaluate(Token[] operands)
+	{
+
+		throwMathLibException("plotyy: not implemented yet");
+
+		return null;
+	}
+}
+
