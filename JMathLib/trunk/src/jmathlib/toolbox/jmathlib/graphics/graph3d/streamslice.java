44076eed4f2f29941a563b19a5304630f76574a1
st_mueller <st_mueller@0cae5b43-3635-0410-beba-a737c0e5ce6c>
2007-08-19 07:40:19 +0000

    Initial import.

---
+++ JMathLib/trunk/src/jmathlib/toolbox/jmathlib/graphics/graph3d/streamslice.java
@@ -0,0 +1,17 @@
+package jmathlib.toolbox.jmathlib.graphics.graph3d;
+
+import jmathlib.core.tokens.*;
+import jmathlib.core.functions.ExternalFunction;
+
+public class streamslice extends ExternalFunction
+{
+
+	public OperandToken evaluate(Token[] operands)
+	{
+
+		throwMathLibException("streamslice: not implemented yet");
+
+		return null;
+	}
+}
+
