99177c4da6017c4012d12713c647f42de55b2299
st_mueller <st_mueller@0cae5b43-3635-0410-beba-a737c0e5ce6c>
2007-08-19 07:38:28 +0000

    Initial import.

---
+++ JMathLib/trunk/src/jmathlib/toolbox/jmathlib/graphics/graph3d/bar3h.java
@@ -0,0 +1,17 @@
+package jmathlib.toolbox.jmathlib.graphics.graph3d;
+
+import jmathlib.core.tokens.*;
+import jmathlib.core.functions.ExternalFunction;
+
+public class bar3h extends ExternalFunction
+{
+
+	public OperandToken evaluate(Token[] operands)
+	{
+
+		throwMathLibException("bar3h: not implemented yet");
+
+		return null;
+	}
+}
+
---
+++ JMathLib/trunk/src/jmathlib/toolbox/jmathlib/graphics/graph3d/contourslice.java
@@ -0,0 +1,17 @@
+package jmathlib.toolbox.jmathlib.graphics.graph3d;
+
+import jmathlib.core.tokens.*;
+import jmathlib.core.functions.ExternalFunction;
+
+public class contourslice extends ExternalFunction
+{
+
+	public OperandToken evaluate(Token[] operands)
+	{
+
+		throwMathLibException("contour3: not implemented yet");
+
+		return null;
+	}
+}
+
