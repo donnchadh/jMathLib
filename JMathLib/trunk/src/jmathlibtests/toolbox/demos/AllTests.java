020fc7dd2eb5a1a586bbb0e2a7d4c4d3d3ac5806
st_mueller <st_mueller@0cae5b43-3635-0410-beba-a737c0e5ce6c>
2007-08-20 18:29:24 +0000

    Initial import.

---
+++ JMathLib/trunk/src/jmathlibtests/toolbox/demos/AllTests.java
@@ -0,0 +1,25 @@
+package jmathlibtests.toolbox.demos;
+
+import jmathlib.tools.junit.framework.*;
+
+/**
+ * TestSuite that runs all the tests
+ *
+ */
+public class AllTests {
+
+	public static void main (String[] args) {
+		jmathlib.tools.junit.textui.TestRunner.run (suite());
+	}
+	public static Test suite ( ) {
+		TestSuite suite= new TestSuite("demo functions");
+
+		/* include subdirectories here */
+		//suite.addTest(MathLib.Tools.TestSuite.Functions.Control.system.AllTests.suite());
+        
+	    /* include tests in this directory here */
+		//suite.addTest(MathLib.Tools.TestSuite.Functions.Control.testXXX.suite());
+
+	    return suite;
+	}
+}
