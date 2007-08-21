38532a554b7b3a084f32f0c42d73c87c294d60c1
st_mueller <st_mueller@0cae5b43-3635-0410-beba-a737c0e5ce6c>
2007-08-21 18:49:57 +0000

    Initial import.

---
+++ JMathLib/trunk/src/jmathlibtests/toolbox/io/testCVSRead.java
@@ -0,0 +1,34 @@
+package jmathlibtests.toolbox.io;
+
+import jmathlib.core.interpreter.Interpreter;
+import jmathlib.tools.junit.framework.*;
+
+public class testCVSRead extends TestCase {
+	protected Interpreter ml;
+	
+    public testCVSRead(String name) {
+		super(name);
+	}
+	public static void main (String[] args) {
+		jmathlib.tools.junit.textui.TestRunner.run (suite());
+	}
+	protected void setUp() {
+		ml = new Interpreter(true);
+	}
+
+	public static Test suite() {
+		return new TestSuite(testCVSRead.class);
+	}
+
+	public void testAbs01() {
+        ml.executeExpression("a=abs(1);");
+		assertTrue(1 == ml.getScalarValueRe("a"));
+	}
+    public void testAbs02() {
+        ml.executeExpression("a=abs(-3);");
+		assertTrue(3 == ml.getScalarValueRe("a"));
+	}
+
+
+
+}
