f5b53b290b2441049421f16d3413e0e8559b95b5
st_mueller <st_mueller@0cae5b43-3635-0410-beba-a737c0e5ce6c>
2007-08-22 17:57:40 +0000

    Initial import.

---
+++ JMathLib/trunk/src/jmathlibtests/toolbox/general/testComplex.java
@@ -0,0 +1,34 @@
+package jmathlibtests.toolbox.general;
+
+import jmathlib.core.interpreter.Interpreter;
+import jmathlib.tools.junit.framework.*;
+
+public class testComplex extends TestCase {
+	protected Interpreter ml;
+	
+    public testComplex(String name) {
+		super(name);
+	}
+	public static void main (String[] args) {
+		jmathlib.tools.junit.textui.TestRunner.run (suite());
+	}
+	protected void setUp() {
+		ml = new Interpreter(true);
+	}
+
+	public static Test suite() {
+		return new TestSuite(testComplex.class);
+	}
+
+	public void testAbs01() {
+        ml.executeExpression("a=abs(1);");
+		assertTrue(1 == ml.getScalarValueRe("a"));
+	}
+    public void testAbs02() {
+        ml.executeExpression("a=abs(-3);");
+		assertTrue(3 == ml.getScalarValueRe("a"));
+	}
+
+
+
+}
---
+++ JMathLib/trunk/src/jmathlibtests/toolbox/general/testWho.java
@@ -0,0 +1,42 @@
+package jmathlibtests.toolbox.general;
+
+import jmathlib.core.interpreter.Interpreter;
+import jmathlib.tools.junit.framework.*;
+
+public class testWho extends TestCase {
+	protected Interpreter ml;
+	
+    public testWho(String name) {
+		super(name);
+	}
+	public static void main (String[] args) {
+		jmathlib.tools.junit.textui.TestRunner.run (suite());
+	}
+	protected void setUp() {
+		ml = new Interpreter(true);
+	}
+
+	public static Test suite() {
+		return new TestSuite(testWho.class);
+	}
+
+	public void testAbs01() {
+        ml.executeExpression("a=abs(1);");
+		assertTrue(1 == ml.getScalarValueRe("a"));
+	}
+    public void testAbs02() {
+        ml.executeExpression("a=abs(-3);");
+		assertTrue(3 == ml.getScalarValueRe("a"));
+	}
+    public void testWho01() {
+        ml.executeExpression("who ; a=3");
+        assertTrue(3 == ml.getScalarValueRe("a"));
+    }
+    public void testWho03() {
+        ml.executeExpression("b=99;d=rand(3,5);who()  ");
+        assertTrue(99 == ml.getScalarValueRe("b"));
+    }
+
+
+
+}
