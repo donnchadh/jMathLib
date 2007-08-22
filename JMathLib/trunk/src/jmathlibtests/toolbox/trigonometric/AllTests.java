8a56a6f084dee2f534d5f3fdf7c614c86e244f54
st_mueller <st_mueller@0cae5b43-3635-0410-beba-a737c0e5ce6c>
2007-08-22 18:56:19 +0000

    Initial import.

--- JMathLib/trunk/src/jmathlibtests/toolbox/trigonometric/AllTests.java
+++ JMathLib/trunk/src/jmathlibtests/toolbox/trigonometric/AllTests.java
@@ -18,7 +18,20 @@ public class AllTests {
 		// none
         
 	    /* include tests in this directory here */
-		suite.addTest(jmathlibtests.toolbox.trigonometric.testDegToGrad.suite());
+        suite.addTest(jmathlibtests.toolbox.trigonometric.testAcos.suite());
+        suite.addTest(jmathlibtests.toolbox.trigonometric.testAcosh.suite());
+        suite.addTest(jmathlibtests.toolbox.trigonometric.testAsin.suite());
+        suite.addTest(jmathlibtests.toolbox.trigonometric.testAsinh.suite());
+        suite.addTest(jmathlibtests.toolbox.trigonometric.testAtan.suite());
+        suite.addTest(jmathlibtests.toolbox.trigonometric.testAtan2.suite());
+        suite.addTest(jmathlibtests.toolbox.trigonometric.testAtanh.suite());
+        suite.addTest(jmathlibtests.toolbox.trigonometric.testCos.suite());
+        suite.addTest(jmathlibtests.toolbox.trigonometric.testCosh.suite());
+        suite.addTest(jmathlibtests.toolbox.trigonometric.testDegToGrad.suite());
+        suite.addTest(jmathlibtests.toolbox.trigonometric.testSin.suite());
+        suite.addTest(jmathlibtests.toolbox.trigonometric.testSinh.suite());
+        suite.addTest(jmathlibtests.toolbox.trigonometric.testTan.suite());
+        suite.addTest(jmathlibtests.toolbox.trigonometric.testTanh.suite());
 
 	    return suite;
 	}
