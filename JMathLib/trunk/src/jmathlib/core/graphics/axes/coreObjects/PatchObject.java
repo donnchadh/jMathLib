36b5d4a63d7e4f80030c058d3aff65d043c9392a
st_mueller <st_mueller@0cae5b43-3635-0410-beba-a737c0e5ce6c>
2007-10-13 08:31:35 +0000

    moved core objects

---
+++ JMathLib/trunk/src/jmathlib/core/graphics/axes/coreObjects/PatchObject.java
@@ -0,0 +1,17 @@
+package jmathlib.core.graphics.axes.coreObjects;
+
+import jmathlib.core.graphics.*;
+//import java.awt.*;
+//import java.awt.event.*;
+//import java.util.Vector;
+
+/** implementation of a line object*/
+public class PatchObject extends GraphicalObject 
+{  
+
+    public PatchObject()
+    {
+    }
+
+
+}
