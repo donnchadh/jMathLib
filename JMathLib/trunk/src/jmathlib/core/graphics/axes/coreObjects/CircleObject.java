e45c25119255570f59d6ae2a6a352beca484e9fd
st_mueller <st_mueller@0cae5b43-3635-0410-beba-a737c0e5ce6c>
2007-10-13 08:33:33 +0000

    moved core objects

---
+++ JMathLib/trunk/src/jmathlib/core/graphics/axes/coreObjects/CircleObject.java
@@ -0,0 +1,19 @@
+package jmathlib.core.graphics.axes.coreObjects;
+
+import jmathlib.core.graphics.*;
+//import java.awt.*;
+//import java.awt.event.*;
+//import java.util.Vector;
+
+/** implementation of a line object*/
+public class CircleObject extends GraphicalObject 
+{  
+
+    public CircleObject()
+    {
+    }
+
+
+}
+
+
