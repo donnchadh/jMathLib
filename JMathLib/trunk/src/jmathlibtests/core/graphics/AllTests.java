9be1d1fae3fc6d342138bedb34aaa8c5e8f5e17e
st_mueller <st_mueller@0cae5b43-3635-0410-beba-a737c0e5ce6c>
2007-10-16 19:30:29 +0000

    more tests

---
+++ JMathLib/trunk/src/jmathlibtests/core/graphics/AllTests.java
@@ -0,0 +1,28 @@
+package jmathlibtests.core.graphics;
+
+import jmathlib.tools.junit.framework.*;
+
+/**
+ * TestSuite that runs all the tests
+ *
+ */
+public class AllTests {
+
+	public static void main (String[] args) {
+	    jmathlib.tools.junit.textui.TestRunner.run (suite());
+	}
+	public static Test suite ( ) {
+		TestSuite suite= new TestSuite("core/graphics");
+
+		/* include subdirectories here */
+		suite.addTest(jmathlibtests.core.graphics.axes.AllTests.suite());
+        suite.addTest(jmathlibtests.core.graphics.properties.AllTests.suite());
+        
+
+		/* include tests in this directory here */
+		//suite.addTest(MathLib.Tools.TestSuite.Interpreter.testParser.suite());
+
+	    return suite;
+	}
+}
+
