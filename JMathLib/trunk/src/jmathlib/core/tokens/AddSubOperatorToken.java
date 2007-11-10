cf66a3e0180e8081a00d86fabd2b590a077543e9
st_mueller <st_mueller@0cae5b43-3635-0410-beba-a737c0e5ce6c>
2007-11-10 18:54:48 +0000

    removed symbolic code

--- JMathLib/trunk/src/jmathlib/core/tokens/AddSubOperatorToken.java
+++ JMathLib/trunk/src/jmathlib/core/tokens/AddSubOperatorToken.java
@@ -1,7 +1,5 @@
 package jmathlib.core.tokens;
 
-//import MathLib.Interpreter.Errors;
-import jmathlib.core.interpreter.RootObject;
 import jmathlib.core.tokens.numbertokens.DoubleNumberToken;
 
 /**Used to implement addition and subtraction operations within an expression*/
@@ -34,141 +32,25 @@ public class AddSubOperatorToken extends BinaryOperatorToken
         if(right == null)
         	right = new DoubleNumberToken(0);
 
-		//try
-		//{
-            OperandToken[] ops = {left, right}; //castOperands(left, right);			
-	        //now evaluate op on left and right        
-	        if(value == '+')
-	        {
-	        	result = ops[0].add(ops[1]);
-	        }
-	        else
-	        {
-	            result = ops[0].subtract(ops[1]);
-	        }
-            
-            if(result == null)
-            {
-                //return origional expression
-                result = new Expression(this, left, right);
-            }
-	 	//}
-	 	//catch(Exception exception)
-	 	//{
-        //    Errors.throwMathLibException("exception in addSub");
-	 	//	result = null;
-	 	//}
+        OperandToken[] ops = {left, right}; //castOperands(left, right);			
+        //now evaluate op on left and right        
+        if(value == '+')
+        {
+        	result = ops[0].add(ops[1]);
+        }
+        else
+        {
+            result = ops[0].subtract(ops[1]);
+        }
+        
+        if(result == null)
+        {
+            //return origional expression
+            result = new Expression(this, left, right);
+        }
 
         return result;
     }
     
-    /**trigonometric functions - calculate the sine of this token
-    @param operands = the operator's operands
-    @return the result as an OperandToken*/
-    public OperandToken sin(OperandToken[] operands)
-    {
-    	//sin(x+y) = sin(x)cos(y) + cos(x)sin(y)
-    	//sin(x-y) = sin(x)cos(y) - cos(x)sin(y)
-    	//x = operands[1]
-    	//y = operands[0]
-    	
-    	OperandToken result = null;
-    	OperandToken left1   = new FunctionToken("cos", operands[1]);
-    	OperandToken left2   = new FunctionToken("sin", operands[0]);
-
-    	OperandToken right1  = new FunctionToken("cos", operands[0]);
-    	OperandToken right2  = new FunctionToken("sin", operands[1]);
-		
-		OperandToken result1 = new Expression(new MulDivOperatorToken('*'), left1, left2);
-
-		OperandToken result2 = new Expression(new MulDivOperatorToken('*'), right1, right2);
-
-		result = new Expression(new AddSubOperatorToken(value), result1, result2);
-		result = result.evaluate(null);
-    	return result;
-    }
-
-    /**trigonometric functions - calculate the cosine of this token
-    @param operands = the operator's operands
-    @return the result as an OperandToken*/
-    public OperandToken cos(OperandToken[] operands)
-    {
-    	//cos(x+y) = cos(x)cos(y) - sin(x)sin(y)
-    	//cos(x-y) = cos(x)cos(y) + sin(x)sin(y)
-    	OperandToken result = null;
-    	OperandToken left1   = new FunctionToken("cos", operands[0]);
-    	OperandToken left2   = new FunctionToken("cos", operands[1]);
-
-    	OperandToken right1  = new FunctionToken("sin", operands[1]);
-    	OperandToken right2  = new FunctionToken("sin", operands[0]);
-		
-		OperandToken result1 = new Expression(new MulDivOperatorToken('*'), left1, left2);
-		OperandToken result2 = new Expression(new MulDivOperatorToken('*'), right1, right2);
-
-		char opToken = '+';
-		if(value == '+')
-			opToken = '-';
-			
-		result = new Expression(new AddSubOperatorToken(opToken), result1, result2);
-		result = result.evaluate(null);
-    	return result;
-    }
-
-    /**trigonometric functions - calculate the tangent of this token
-    @param operands = the operator's operands
-    @return the result as an OperandToken*/
-    public OperandToken tan(OperandToken[] operands)
-    {
-    	//tan(iy) = sin(iy) / cos(iy)
-		OperandToken cosine = cos(operands);
-		OperandToken sine   = sin(operands);
-    	
-    	OperandToken result = new Expression(new MulDivOperatorToken('/'), cosine, sine);
-    	result = result.evaluate(null);
-    	
-        return result;   	
-    }
-
-    /**Standard functions - calculates the exponent
-    @param operands = the operator's operands
-    @return the result as an OperandToken*/
-    public OperandToken exp(OperandToken[] operands)
-    {
-    	//exp(x+y) = exp(x) * exp(y)
-    	OperandToken left   = new FunctionToken("exp", operands[0]);
-    	OperandToken right  = new FunctionToken("exp", operands[1]);
-    	char opToken = '*';
-    	if(value == '-')
-    		opToken = '/';
-    		
-    	OperandToken result = new Expression(new MulDivOperatorToken(opToken), left, right);
-
-    	result = result.evaluate(null);
-    	return result;
-    }
-
-    /**Standard functions - calculates the natural logarythm
-    @param operands = the operator's operands
-    @return the result as an OperandToken*/
-    public OperandToken ln(OperandToken[] operands)
-    {
-    	return null;
-    }
-
-    /**Standard functions - calculates the logarythm
-    @param operands = the operator's operands
-    @return the result as an OperandToken*/
-    public OperandToken log(OperandToken[] operands, RootObject arg)
-    {
-    	return null;
-    }
-
-	/**Standard functions - calculates the square root    
-    @param operands = the operator's operands
-    @return the result as an OperandToken*/
-    public OperandToken sqrt(OperandToken[] operands)
-    {
-    	return null;
-    }
     
 }
