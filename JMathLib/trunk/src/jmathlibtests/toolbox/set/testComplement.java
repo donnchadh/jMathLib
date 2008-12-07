3e6d6291ee7c141bfc2c1feba90a808511ba0a50
st_mueller <st_mueller@0cae5b43-3635-0410-beba-a737c0e5ce6c>
2008-12-07 18:30:51 +0000

    added tearDown() method to test cases

--- JMathLib/trunk/src/jmathlibtests/toolbox/set/testComplement.java
+++ JMathLib/trunk/src/jmathlibtests/toolbox/set/testComplement.java
@@ -16,6 +16,9 @@ public class testComplement extends TestCase {
 	protected void setUp() {
 		ml = new Interpreter(true);
 	}
+    protected void tearDown() {
+        ml = null;
+    }
 
 	public static Test suite() {
 		return new TestSuite(testComplement.class);
--- JMathLib/trunk/src/jmathlibtests/toolbox/specfun/testPerms.java
+++ JMathLib/trunk/src/jmathlibtests/toolbox/specfun/testPerms.java
@@ -16,6 +16,9 @@ public class testPerms extends TestCase {
 	protected void setUp() {
 		ml = new Interpreter(true);
 	}
+    protected void tearDown() {
+        ml = null;
+    }
 
 	public static Test suite() {
 		return new TestSuite(testPerms.class);
