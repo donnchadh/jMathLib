56e58bb1a13bc99f8a1a40b67726d2d6d5c94341
st_mueller <st_mueller@0cae5b43-3635-0410-beba-a737c0e5ce6c>
2009-01-05 13:07:14 +0000

    changed handling of global variables

--- JMathLib/trunk/src/jmathlibtests/toolbox/jmathlib/matrix/testChol.java
+++ JMathLib/trunk/src/jmathlibtests/toolbox/jmathlib/matrix/testChol.java
@@ -1,6 +1,7 @@
 package jmathlibtests.toolbox.jmathlib.matrix;
 
 import jmathlib.core.interpreter.Interpreter;
+import jmathlib.core.interpreter.*;
 import jmathlib.tools.junit.framework.*;
 import jmathlibtests.Compare;
 
@@ -34,11 +35,22 @@ public class testChol extends TestCase {
 		assertTrue(ml.getScalarValueBoolean("aaa"));
 	}
     public void testNot03() {
-        double[][] a = {{1.0, 2.0, 3.0},{-1.0, 0.0, 0.0},{11.23, -1.5, 0.0}};
-        boolean[][] b = {{false, false, false},{ false, true, true},{ false,false, true}};
+        ErrorLogger.debugLine("fffffaa a00= "+ml.getArrayValueRe("a")[0][0]);
+        double[][] a = {{111.0, 2.0, 3.0},{-1.0, 0.0, 0.0},{11.23, -1.5, 0.0}};
+        boolean[][] b = {{false, false, false},{ false, true, true},{ false, false, true}};
         ml.setArray("a", a, null);
-        ml.executeExpression("setdebug(1)");
+        ErrorLogger.debugLine("fffffaa a00= "+ml.getArrayValueRe("a")[0][0]);
+        
+        //ml.executeExpression("setdebug(1)");
+        //ml.executeExpression("a");
         ml.executeExpression("who, z = not(a)");
+        double[][] v = ml.getArrayValueRe("a");
+        boolean[][] vv = ml.getArrayValueBoolean("z");
+        for (int y=0; y<v.length; y++)
+            for (int x=0; x<v[0].length; x++ )
+                ErrorLogger.debugLine("v = "+y+" "+x+" "+vv[y][x]+" "+v[y][x]);
+        
+        ErrorLogger.debugLine("fffffaa"); 
  		assertTrue(Compare.ArrayEquals(b, ml.getArrayValueBoolean("z")));
 	}
 
