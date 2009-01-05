e046cd4043457801dc88b7dfc07b19471f12bb4f
st_mueller <st_mueller@0cae5b43-3635-0410-beba-a737c0e5ce6c>
2009-01-05 15:02:49 +0000

    changed handling of global variables

--- JMathLib/trunk/src/jmathlib/core/interpreter/ContextList.java
+++ JMathLib/trunk/src/jmathlib/core/interpreter/ContextList.java
@@ -1,24 +1,26 @@
 package jmathlib.core.interpreter;
 
+import jmathlib.core.tokens.OperandToken;
+
 
 /**Stores a list of contexts. A context is a set of variables pluss some code.
    When a function is called a new context is added to the list. When the function is removed, the context is destroyed. */
 public class ContextList
 {
     // reference to the currently executing context
-    private Context currentContext;
+    private Context localContext;
     
     // reference to the global context
     private Context globalContext;
     
     /**Create an empty context to act as the global context
-     * This constructor should only be called ONCE, because it also creates the
+     * This constructor must only be called ONCE, because it also creates the
      * GLOBAL context
      */
     public ContextList()
     {
         globalContext  = new Context(); // global context 
-        currentContext = new Context(); // local context
+        localContext   = new Context(); // local context
     }
 
     /**
@@ -27,11 +29,11 @@ public class ContextList
      */
     public void pushContext(Context context)
     {
-        context.setParent(currentContext);
-        currentContext = context; 
+        context.setParent(localContext);
+        localContext = context; 
     }
 
-    /**Used to create a new context and put it on the stack when executing a function
+    /** Used to create a new context and put it on the stack when executing a function
      * @param _variables = the variable list of the function, null if it's a script file
      */
     public Context createContext(VariableList _variables)
@@ -40,14 +42,14 @@ public class ContextList
         if(_variables == null)
         {
             //use the calling functions variable list
-            _variables = currentContext.getVariables();
+            _variables = localContext.getVariables();
         }
                    
         //create a new context and make it the current context
-        //currentContext = new Context(_variables, null, currentContext);
-        currentContext = new Context(_variables, currentContext);
+        //localContext = new Context(_variables, null, localContext);
+        localContext = new Context(_variables, localContext);
 
-        return currentContext;
+        return localContext;
     }
     
     /**
@@ -55,17 +57,17 @@ public class ContextList
      */
     public Context popContext()
     {       
-        Context context = currentContext;
-        currentContext = currentContext.getParent();
+        Context context = localContext;
+        localContext    = localContext.getParent();
         return context; 
     }
 
     /**
      * @return the variable list of the current context
      */
-    public VariableList getVariables()
+    public VariableList getLocalVariables()
     {
-        return currentContext.getVariables();
+        return localContext.getVariables();
     }
 
     /**
@@ -77,14 +79,16 @@ public class ContextList
     }
 
     /**
-     * 
+     * this method returns a variable from the workspace. It also checks
+     * if the variable is local or global and returns the value from
+     * the correct local or global workspace.
      * @param name
      * @return
      */
     public Variable getVariable(String name)
     {
         // check if variable is marked as global in current context
-        Variable var = currentContext.getVariables().getVariable(name);
+        Variable var = localContext.getVariables().getVariable(name);
         if ((var!=null) && (var.isGlobal()))
         {
             // variable is marked global
@@ -95,8 +99,46 @@ public class ContextList
         {
             // variable is local only
             // return data from current context
-            return currentContext.getVariables().getVariable(name);
+            return localContext.getVariables().getVariable(name);
         }
+    }
+    
+    /**
+     * create a variable in the local or global workspace
+     * @param
+     * @return
+     */
+    public Variable createVariable(String name)
+    {
+        // check if variable is already created in local or global workspace
+        Variable var = getVariable(name);
+        
+        // return if variable is already created
+        if (getVariable(name)!=null)
+            return var;
+        
+        // create a local variable
+        return getLocalVariables().createVariable(name);
+        
+    }
+
+    /**
+     * Set a variable in the local or global workspace. In case
+     * the variable is not yet created, create it, too.
+     * @param
+     * @param
+     */
+    public void setVariable(String name, OperandToken value)
+    {
+        // try to get variable from local or global context
+        Variable var = getVariable(name);
+        
+        // if variable is not yet available, create it
+        if (var==null)
+            createVariable(name);
+     
+        // assign value to newly created variable
+        getVariable(name).assign(value);
     }
 
     /**
@@ -104,15 +146,15 @@ public class ContextList
      */
     public boolean isGlobalContext()
     {
-        return (currentContext.getParent() == null);
+        return (localContext.getParent() == null);
     }
 
     /**
      * 
      * @return
      */
-    public Context getCurrentContext()
+    public Context getLocalContext()
     {
-        return currentContext;
+        return localContext;
     }
 }
