946a437b21bd06d8a984180a52f53931e26e7faa
st_mueller <st_mueller@0cae5b43-3635-0410-beba-a737c0e5ce6c>
2009-01-06 13:17:03 +0000

    more directories for tests

---
+++ JMathLib/trunk/src/jmathlibtests/toolbox/ftp/AllTests.java
@@ -0,0 +1,25 @@
+package jmathlibtests.toolbox.ftp;
+
+import jmathlib.tools.junit.framework.*;
+
+/**
+ * TestSuite that runs all the tests
+ *
+ */
+public class AllTests {
+
+	public static void main (String[] args) {
+		jmathlib.tools.junit.textui.TestRunner.run (suite());
+	}
+	public static Test suite ( ) {
+		TestSuite suite= new TestSuite("ftp functions");
+
+		/* include subdirectories here */
+		// none
+        
+	    /* include tests in this directory here */
+		// none yet
+		
+	    return suite;
+	}
+}
