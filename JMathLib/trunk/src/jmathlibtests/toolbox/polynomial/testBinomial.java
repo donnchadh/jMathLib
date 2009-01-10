a35978bfb1241ecaf9d352b86ce632c48ed0c612
st_mueller <st_mueller@0cae5b43-3635-0410-beba-a737c0e5ce6c>
2009-01-10 15:01:05 +0000

    adde more tests

--- JMathLib/trunk/src/jmathlibtests/toolbox/polynomial/testBinomial.java
+++ JMathLib/trunk/src/jmathlibtests/toolbox/polynomial/testBinomial.java
@@ -3,8 +3,7 @@ package jmathlibtests.toolbox.polynomial;
 import jmathlib.core.interpreter.Interpreter;
 import jmathlib.tools.junit.framework.*;
 
-public class testBinomial extends TestCase {
-	protected Interpreter ml;
+public class testBinomial extends JMathLibTestCase {
 	
     public testBinomial(String name) {
 		super(name);
@@ -12,26 +11,41 @@ public class testBinomial extends TestCase {
 	public static void main (String[] args) {
 		jmathlib.tools.junit.textui.TestRunner.run (suite());
 	}
-	protected void setUp() {
-		ml = new Interpreter(true);
-	}
-
-    protected void tearDown() {
-        ml = null;
-    }
 
 	public static Test suite() {
 		return new TestSuite(testBinomial.class);
 	}
 
-	public void testAbs01() {
-        ml.executeExpression("a=abs(1);");
-		assertTrue(1 == ml.getScalarValueRe("a"));
+	public void testBinomial01() {
+        assertEvalArrayEquals("a=binomial(2);", 
+                "a", 
+                new double[][] {{1, 2, 1}}, 
+                0.001); 
 	}
-    public void testAbs02() {
-        ml.executeExpression("a=abs(-3);");
-		assertTrue(3 == ml.getScalarValueRe("a"));
+    public void testBinomial02() {
+		assertEvalArrayEquals("a=binomial(3);", 
+		        "a", 
+		        new double[][] {{1, 3, 3, 1}}, 
+		        0.001); 
 	}
+    public void testBinomial03() {
+        assertEvalArrayEquals("a=binomial(4);", 
+                "a", 
+                new double[][] {{1, 4, 6, 4, 1}}, 
+                0.001); 
+    }
+    public void testBinomial04() {
+        assertEvalArrayEquals("a=binomial(5);", 
+                "a", 
+                new double[][] {{1, 5, 10, 10, 5, 1}}, 
+                0.001); 
+    }
+    public void testBinomial05() {
+        assertEvalArrayEquals("a=binomial(6);", 
+                "a", 
+                new double[][] {{1, 6, 15, 20, 15, 6, 1}}, 
+                0.001); 
+    }
 
 
 
