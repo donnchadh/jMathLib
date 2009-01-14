6ba2fe52e9a8dd57dcfd1625ad7c95f6f7e971c6
st_mueller <st_mueller@0cae5b43-3635-0410-beba-a737c0e5ce6c>
2009-01-14 20:37:21 +0000

    code clean up. new JMathLibTestCase.java

--- JMathLib/trunk/src/jmathlibtests/scripts/test_jmathlib_toolbox_general_uint32.java
+++ JMathLib/trunk/src/jmathlibtests/scripts/test_jmathlib_toolbox_general_uint32.java
@@ -4,8 +4,7 @@ import jmathlib.core.interpreter.Interpreter;
 import jmathlib.tools.junit.framework.*;        
 import jmathlibtests.Compare;                   
 
-public class test_jmathlib_toolbox_general_uint32 extends TestCase {     
-    protected Interpreter ml;                   
+public class test_jmathlib_toolbox_general_uint32 extends JMathLibTestCase {     
      
     public test_jmathlib_toolbox_general_uint32(String name) {           
         super(name);                            
@@ -13,12 +12,6 @@ public class test_jmathlib_toolbox_general_uint32 extends TestCase {
     public static void main (String[] args) {   
         jmathlib.tools.junit.textui.TestRunner.run (suite()); 
     }                                           
-    protected void setUp() {                    
-        ml = new Interpreter(true);             
-    }                                           
-    protected void tearDown() {                 
-        ml = null;                              
-    } 
  
     public static Test suite() {                
         return new TestSuite(test_jmathlib_toolbox_general_uint32.class); 
--- JMathLib/trunk/src/jmathlibtests/scripts/test_jmathlib_toolbox_jmathlib_internal_j.java
+++ JMathLib/trunk/src/jmathlibtests/scripts/test_jmathlib_toolbox_jmathlib_internal_j.java
@@ -4,8 +4,7 @@ import jmathlib.core.interpreter.Interpreter;
 import jmathlib.tools.junit.framework.*;        
 import jmathlibtests.Compare;                   
 
-public class test_jmathlib_toolbox_jmathlib_internal_j extends TestCase {     
-    protected Interpreter ml;                   
+public class test_jmathlib_toolbox_jmathlib_internal_j extends JMathLibTestCase {     
      
     public test_jmathlib_toolbox_jmathlib_internal_j(String name) {           
         super(name);                            
@@ -13,12 +12,6 @@ public class test_jmathlib_toolbox_jmathlib_internal_j extends TestCase {
     public static void main (String[] args) {   
         jmathlib.tools.junit.textui.TestRunner.run (suite()); 
     }                                           
-    protected void setUp() {                    
-        ml = new Interpreter(true);             
-    }                                           
-    protected void tearDown() {                 
-        ml = null;                              
-    } 
  
     public static Test suite() {                
         return new TestSuite(test_jmathlib_toolbox_jmathlib_internal_j.class); 
