022ed589eff9c91ac996fff8726a8a6087e72b8b
st_mueller <st_mueller@0cae5b43-3635-0410-beba-a737c0e5ce6c>
2009-01-20 20:19:48 +0000

    changed signature of evaluate(Token[] operands) to evaluate(Token[] operands, GlobalValues globals)

--- JMathLib/trunk/src/jmathlib/toolbox/jmathlib/graphics/graph2d/ezcontourf.java
+++ JMathLib/trunk/src/jmathlib/toolbox/jmathlib/graphics/graph2d/ezcontourf.java
@@ -2,11 +2,12 @@ package jmathlib.toolbox.jmathlib.graphics.graph2d;
 
 import jmathlib.core.tokens.*;
 import jmathlib.core.functions.ExternalFunction;
+import jmathlib.core.interpreter.GlobalValues;
 
 public class ezcontourf extends ExternalFunction
 {
 
-	public OperandToken evaluate(Token[] operands)
+	public OperandToken evaluate(Token[] operands, GlobalValues globals)
 	{
 
 		throwMathLibException("ezcontourf: not implemented yet");
