213d76cafab8d32cc71a2953d881b887c7f7ceb7
st_mueller <st_mueller@0cae5b43-3635-0410-beba-a737c0e5ce6c>
2009-01-21 19:28:12 +0000

    changed signature of evaluate(Token[] operands) to evaluate(Token[] operands, GlobalValues globals)

--- JMathLib/trunk/src/jmathlib/toolbox/jmathlib/graphics/graph3d/ellipsoid.java
+++ JMathLib/trunk/src/jmathlib/toolbox/jmathlib/graphics/graph3d/ellipsoid.java
@@ -2,11 +2,12 @@ package jmathlib.toolbox.jmathlib.graphics.graph3d;
 
 import jmathlib.core.tokens.*;
 import jmathlib.core.functions.ExternalFunction;
+import jmathlib.core.interpreter.GlobalValues;
 
 public class ellipsoid extends ExternalFunction
 {
 
-	public OperandToken evaluate(Token[] operands)
+	public OperandToken evaluate(Token[] operands, GlobalValues globals)
 	{
 
 		throwMathLibException("ellipsoid: not implemented yet");
--- JMathLib/trunk/src/jmathlib/toolbox/jmathlib/graphics/graph3d/ezplot3.java
+++ JMathLib/trunk/src/jmathlib/toolbox/jmathlib/graphics/graph3d/ezplot3.java
@@ -2,11 +2,12 @@ package jmathlib.toolbox.jmathlib.graphics.graph3d;
 
 import jmathlib.core.tokens.*;
 import jmathlib.core.functions.ExternalFunction;
+import jmathlib.core.interpreter.GlobalValues;
 
 public class ezplot3 extends ExternalFunction
 {
 
-	public OperandToken evaluate(Token[] operands)
+	public OperandToken evaluate(Token[] operands, GlobalValues globals)
 	{
 
 		throwMathLibException("ezplot3: not implemented yet");
