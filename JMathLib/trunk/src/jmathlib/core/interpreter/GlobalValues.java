5be104398e44812edb39627bcb838c46c3cc632b
st_mueller <st_mueller@0cae5b43-3635-0410-beba-a737c0e5ce6c>
2009-01-23 20:33:31 +0000

    changed signature of evaluate(Token[] operands) to evaluate(Token[] operands, GlobalValues globals)

--- JMathLib/trunk/src/jmathlib/core/interpreter/GlobalValues.java
+++ JMathLib/trunk/src/jmathlib/core/interpreter/GlobalValues.java
@@ -21,28 +21,28 @@ These values are all private and should be access through the getter/setter func
 public class GlobalValues
 {
     /**A list of contexts*/
-    static transient private ContextList contextList;
+    private ContextList contextList;
     
     /**Object to control function usage*/
-    static transient private jmathlib.core.functions.FunctionManager functionManager;
+    private jmathlib.core.functions.FunctionManager functionManager;
 
     /**Class Loader for loading classes for handling casting*/
     //static transient private MathLib.Casts.CastClassLoader castClassLoader; 
 
     /**A pointer to the interpreter itself*/
-    static transient private Interpreter interpreter;
+    private Interpreter interpreter;
     
     /**Object to control graphical output*/
-    static transient private jmathlib.core.graphics.GraphicsManager graphicsManager;  
+    private jmathlib.core.graphics.GraphicsManager graphicsManager;  
 
     /**Object to control plugins */
-    static transient private jmathlib.plugins.PluginsManager pluginsManager;     
+    private jmathlib.plugins.PluginsManager pluginsManager;     
 
     /**sets whether to write debug lines to console and log files*/
-    static transient private boolean debug = true;
+    private boolean debug = true;
     
     /**stores the number format for displaying the number*/
-    public transient static NumberFormat numFormat = new DecimalFormat("0.0000", new DecimalFormatSymbols(Locale.ENGLISH));
+    public /*static transient*/ NumberFormat numFormat = new DecimalFormat("0.0000", new DecimalFormatSymbols(Locale.ENGLISH));
 
     /**Initialises the global values
        @param _interpreter = the Interpreter object
@@ -70,104 +70,120 @@ public class GlobalValues
          
     }   
 
-    /**@return the current variable list*/
-    protected VariableList getLocalVariables()
+    /**
+     * @return the current variable list
+     */
+    public VariableList getLocalVariables()
     {
         return contextList.getLocalVariables();
     }
 
-    /**@return the global variable list*/
-    protected VariableList getGlobalVariables()
+    /**
+     * @return the global variable list
+     */
+    public VariableList getGlobalVariables()
     {
         return contextList.getGlobalVariables();
     }
 
-    /**@return the a variable from local or global workspace*/
-    protected Variable getVariable(String name)
+    /**
+     * @param
+     * @return the a variable from local or global workspace
+     */
+    public Variable getVariable(String name)
     {
         return contextList.getVariable(name);
     }
 
-    /** create a variable in the local or global workspace*/
-    protected Variable createVariable(String name)
+    /** create a variable in the local or global workspace
+     * @param
+     * @return
+     */
+    public Variable createVariable(String name)
     {
         return contextList.createVariable(name);
     }
 
-    /** create a variable in the local or global workspace*/
-    protected void setVariable(String name, OperandToken value)
+    /** create a variable in the local or global workspace
+     * @param
+     * @param
+     */
+    public void setVariable(String name, OperandToken value)
     {
         contextList.setVariable(name, value);
     }
 
     /**Change the current context to point to the new Variable List
-    @param _Variables = the new list of Variables to use*/
-    protected void createContext(VariableList _variables)
+     * @param _Variables = the new list of Variables to use
+     */
+    public void createContext(VariableList _variables)
     {
         contextList.createContext(_variables);
     }
 
-    /**Return to the previous variable list*/
-    protected void popContext()
+    /**Return to the previous variable list
+     */
+    public void popContext()
     {
         contextList.popContext();
     }
     
-    /**Allow access to the context list*/    
-    protected ContextList getContextList()
+    /**Allow access to the context list
+     * @return
+     */    
+    public ContextList getContextList()
     {
         return contextList;
     }
 
-    /**@return the interpreter object*/
-    protected Interpreter getInterpreter()
+    /**@return the interpreter object
+     * @return
+     */
+    public Interpreter getInterpreter()
     {
         return interpreter;
     }
 
-    /**@return the function manager*/
-    protected jmathlib.core.functions.FunctionManager getFunctionManager()
+    /**
+     * @return the function manager
+     */
+    public jmathlib.core.functions.FunctionManager getFunctionManager()
     {
         return functionManager;
     }
 
-    /**@return handle to graphics manager*/
-    protected jmathlib.core.graphics.GraphicsManager getGraphicsManager()
+    /**
+     * @return handle to graphics manager
+     */
+    public jmathlib.core.graphics.GraphicsManager getGraphicsManager()
     {
         return graphicsManager;
     }
 
-    /**@return handle to plugins manager*/
-    protected jmathlib.plugins.PluginsManager getPluginsManager()
+    /**
+     * @return handle to plugins manager
+     */
+    public jmathlib.plugins.PluginsManager getPluginsManager()
     {
         return pluginsManager;
     }
 
-    /** @return actual working directory */
-    protected File getWorkingDirectory()
+    /** 
+     * @return actual working directory 
+     */
+    public File getWorkingDirectory()
     {
         return functionManager.getWorkingDirectory();
     }
 
-    /** @param set working directory */
-    protected void setWorkingDirectory(File _workingDir)
+    /** 
+     * @param set working directory 
+     */
+    public void setWorkingDirectory(File _workingDir)
     {
         functionManager.setWorkingDirectory(_workingDir);
     }
 
-    /**@return the setting of the debug flag*/
-    public boolean getDebug()
-    {
-        return debug;
-    }
-    
-    /**sets the debug flag
-    @param _debug = should debug information be displayed*/
-    public void setDebug(boolean _debug)
-    {
-        debug= _debug;
-    }
-    
     /**
      * returns the number format for displaying numbers
      * @return format type
