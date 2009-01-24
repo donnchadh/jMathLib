d2a6dcef6ad79fb46d27ba06c65a7ec916ef4b87
st_mueller <st_mueller@0cae5b43-3635-0410-beba-a737c0e5ce6c>
2009-01-24 09:29:27 +0000

    changed signature of evaluate(Token[] operands) to evaluate(Token[] operands, GlobalValues globals)

--- JMathLib/trunk/src/jmathlib/core/tokens/SwitchToken.java
+++ JMathLib/trunk/src/jmathlib/core/tokens/SwitchToken.java
@@ -1,6 +1,8 @@
 package jmathlib.core.tokens;
 
 import jmathlib.core.interpreter.ErrorLogger;
+import jmathlib.core.interpreter.GlobalValues;
+
 import java.util.Vector;
 
 
@@ -15,28 +17,39 @@ public class SwitchToken extends CommandToken
 	Vector cases;
 
 	/**Constructor setting cases
-	@param _value = an OperandToken containing the value to test
-	@param _cases = a vector of case tokens*/
+	 * @param _value = an OperandToken containing the value to test
+	 * @param _cases = a vector of case tokens
+	 */
 	public SwitchToken(OperandToken _value, Vector _cases)
 	{
 		value = _value;
 		cases = _cases;
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
 	public OperandToken getData()
 	{
 		return value;
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
 	public Vector getCases()
 	{
 		return cases;
 	}
 
     /**evaluates the operator
-    @param operands = the operators operands
-    @return the result of the test as an OperandToken*/
-    public OperandToken evaluate(Token[] operands)
+     * @param operands = the operators operands
+     * @param
+     * @return the result of the test as an OperandToken
+     */
+    public OperandToken evaluate(Token[] operands, GlobalValues globals)
     {
     	OperandToken result = null;
     	
@@ -47,7 +60,7 @@ public class SwitchToken extends CommandToken
     		CaseToken caseToken = ((CaseToken)cases.elementAt(pos));
     		ErrorLogger.debugLine("switch "+caseToken.toString());
     		
-    		result = caseToken.evaluate(new OperandToken[]{value});
+    		result = caseToken.evaluate(new OperandToken[]{value}, globals);
     		
     		pos++;
     	}
@@ -57,7 +70,8 @@ public class SwitchToken extends CommandToken
     
 
     /**Convert the operator to a string
-    @return the operator as a string*/
+     * @return the operator as a string
+     */
     public String toString()
     {
         return "switch"; //( "+value.toString()+" )";
