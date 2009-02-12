7fdf1b5d98bd6e197b16d1810efffaf8855e32bf
st_mueller <st_mueller@0cae5b43-3635-0410-beba-a737c0e5ce6c>
2009-02-12 21:13:15 +0000

    added some comments

--- JMathLib/trunk/src/jmathlib/core/interpreter/RootObject.java
+++ JMathLib/trunk/src/jmathlib/core/interpreter/RootObject.java
@@ -1,3 +1,11 @@
+/* 
+ * This file is part or JMathLib 
+ * 
+ * Check it out at http://www.jmathlib.de
+ *
+ * Author:   
+ * (c) 2005-2009   
+ */
 package jmathlib.core.interpreter;
 
 import java.io.*;
