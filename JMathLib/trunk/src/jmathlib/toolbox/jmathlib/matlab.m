bcbd2c74a6611be7d509de5eb0842755ce85fb93
st_mueller <st_mueller@0cae5b43-3635-0410-beba-a737c0e5ce6c>
2009-02-26 10:11:08 +0000

    added weblink

--- JMathLib/trunk/src/jmathlib/toolbox/jmathlib/matlab.m
+++ JMathLib/trunk/src/jmathlib/toolbox/jmathlib/matlab.m
@@ -10,6 +10,7 @@ Matlab is a great tool. JMathLib is a clone of Matlab, but written 100% in java.
 @EXAMPLES
 .
 @NOTES
+You may find matlab at http://www.mathworks.com
 @SEE
 jmathlib, scilab, freemat, octave
 */
