6a4a3ac32e5567a50c29c13239d0f265ab5f468a
st_mueller <st_mueller@0cae5b43-3635-0410-beba-a737c0e5ce6c>
2009-02-26 10:11:22 +0000

    added weblink

--- JMathLib/trunk/src/jmathlib/toolbox/jmathlib/octave.m
+++ JMathLib/trunk/src/jmathlib/toolbox/jmathlib/octave.m
@@ -10,6 +10,7 @@ Octave is a great tool. JMathLib is a clone of Octave, but written 100% in java.
 @EXAMPLES
 .
 @NOTES
+You may find octave at www.octave.org
 @SEE
 jmathlib, matlab, freemat, scilab
 */
