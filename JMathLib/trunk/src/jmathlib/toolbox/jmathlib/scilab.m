5df75537d3e1497439df0b17dcc081b86f81dc59
st_mueller <st_mueller@0cae5b43-3635-0410-beba-a737c0e5ce6c>
2009-02-26 10:12:16 +0000

    added weblink

--- JMathLib/trunk/src/jmathlib/toolbox/jmathlib/scilab.m
+++ JMathLib/trunk/src/jmathlib/toolbox/jmathlib/scilab.m
@@ -10,6 +10,7 @@ Scilab is a great tool. JMathLib is a clone of Scilab, but written 100% in java.
 @EXAMPLES
 .
 @NOTES
+You may found Scilab at www.scilab.org
 @SEE
 jmathlib, matlab, freemat, octave
 */
